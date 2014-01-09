5cd349ff3aaf4cdeba70c49631d2dbc90d1f1133
Rhizome.org <smeisburger@gmail.com>
2014-01-09 14:21:03 -0500

    added setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-eazy-email',
+      version='0.1',
+      description='',
+      author='Scott Meisburger',
+      author_email='smeisburger@gmail.com',
+      url='https://github.com/rhizomedotorg/django-eazy-email',
+      packages=['django_eazy_email'],
+     )
