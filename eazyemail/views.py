81c983428cf735c13f6d47751cb64aa3c3013d69
Rhizome.org <smeisburger@gmail.com>
2014-01-16 16:23:15 -0500

    changed text email preview content type

--- eazyemail/views.py
+++ eazyemail/views.py
@@ -8,7 +8,7 @@ def preview(request, slug):
     email = get_object_or_404(EazyEmail, slug=slug)
 
     if email.html_body:
-        content = email.html_content(email.dummy_data_dict)
-    else:
-        content = email.render_text_body(email.dummy_data_dict)
-    return HttpResponse(content)
+        return HttpResponse(email.html_content(email.dummy_data_dict))
+
+    content = email.render_text_body(email.dummy_data_dict)
+    return HttpResponse(content, content_type='text/plain')
